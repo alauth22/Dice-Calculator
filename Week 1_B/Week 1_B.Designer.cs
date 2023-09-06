7c711747f2d7ef0ede9aef1d0ba776997487fc8c
25136574 <25136574@STUWIN10-17>
2023-09-06 12:28:08 -0500

    Add project files.

---
+++ Week 1_B/Week 1_B.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace Week_1_B
+{
+    partial class frmNumbersCalculations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNumbersCalculations));
+            this.pbsDice2 = new System.Windows.Forms.PictureBox();
+            this.pbxDice1 = new System.Windows.Forms.PictureBox();
+            this.pbxDice4 = new System.Windows.Forms.PictureBox();
+            this.pbxDice3 = new System.Windows.Forms.PictureBox();
+            this.lblDice1Count = new System.Windows.Forms.Label();
+            this.lblDice4Count = new System.Windows.Forms.Label();
+            this.lblTotalCount = new System.Windows.Forms.Label();
+            this.lblActualTotalValue = new System.Windows.Forms.Label();
+            this.lblActualTotalCount = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.lblDice2Count = new System.Windows.Forms.Label();
+            this.lblDice3Count = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbsDice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbsDice2
+            // 
+            this.pbsDice2.Image = ((System.Drawing.Image)(resources.GetObject("pbsDice2.Image")));
+            this.pbsDice2.Location = new System.Drawing.Point(117, 240);
+            this.pbsDice2.Name = "pbsDice2";
+            this.pbsDice2.Size = new System.Drawing.Size(107, 107);
+            this.pbsDice2.TabIndex = 0;
+            this.pbsDice2.TabStop = false;
+            this.pbsDice2.Click += new System.EventHandler(this.pbsDice2_Click);
+            // 
+            // pbxDice1
+            // 
+            this.pbxDice1.Image = ((System.Drawing.Image)(resources.GetObject("pbxDice1.Image")));
+            this.pbxDice1.Location = new System.Drawing.Point(118, 38);
+            this.pbxDice1.Name = "pbxDice1";
+            this.pbxDice1.Size = new System.Drawing.Size(106, 104);
+            this.pbxDice1.TabIndex = 1;
+            this.pbxDice1.TabStop = false;
+            this.pbxDice1.Click += new System.EventHandler(this.pbxDice1_Click);
+            // 
+            // pbxDice4
+            // 
+            this.pbxDice4.Image = ((System.Drawing.Image)(resources.GetObject("pbxDice4.Image")));
+            this.pbxDice4.Location = new System.Drawing.Point(299, 240);
+            this.pbxDice4.Name = "pbxDice4";
+            this.pbxDice4.Size = new System.Drawing.Size(106, 107);
+            this.pbxDice4.TabIndex = 2;
+            this.pbxDice4.TabStop = false;
+            this.pbxDice4.Click += new System.EventHandler(this.pbxDice4_Click);
+            // 
+            // pbxDice3
+            // 
+            this.pbxDice3.Image = ((System.Drawing.Image)(resources.GetObject("pbxDice3.Image")));
+            this.pbxDice3.Location = new System.Drawing.Point(299, 39);
+            this.pbxDice3.Name = "pbxDice3";
+            this.pbxDice3.Size = new System.Drawing.Size(106, 103);
+            this.pbxDice3.TabIndex = 3;
+            this.pbxDice3.TabStop = false;
+            this.pbxDice3.Click += new System.EventHandler(this.pbxDice3_Click);
+            // 
+            // lblDice1Count
+            // 
+            this.lblDice1Count.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDice1Count.Location = new System.Drawing.Point(130, 153);
+            this.lblDice1Count.Name = "lblDice1Count";
+            this.lblDice1Count.Size = new System.Drawing.Size(73, 23);
+            this.lblDice1Count.TabIndex = 4;
+            this.lblDice1Count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice4Count
+            // 
+            this.lblDice4Count.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDice4Count.Location = new System.Drawing.Point(315, 360);
+            this.lblDice4Count.Name = "lblDice4Count";
+            this.lblDice4Count.Size = new System.Drawing.Size(73, 23);
+            this.lblDice4Count.TabIndex = 5;
+            this.lblDice4Count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalCount
+            // 
+            this.lblTotalCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalCount.Location = new System.Drawing.Point(467, 39);
+            this.lblTotalCount.Name = "lblTotalCount";
+            this.lblTotalCount.Size = new System.Drawing.Size(82, 29);
+            this.lblTotalCount.TabIndex = 6;
+            this.lblTotalCount.Text = "Total Count";
+            this.lblTotalCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActualTotalValue
+            // 
+            this.lblActualTotalValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblActualTotalValue.Location = new System.Drawing.Point(466, 176);
+            this.lblActualTotalValue.Name = "lblActualTotalValue";
+            this.lblActualTotalValue.Size = new System.Drawing.Size(82, 23);
+            this.lblActualTotalValue.TabIndex = 7;
+            this.lblActualTotalValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActualTotalCount
+            // 
+            this.lblActualTotalCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblActualTotalCount.Location = new System.Drawing.Point(468, 83);
+            this.lblActualTotalCount.Name = "lblActualTotalCount";
+            this.lblActualTotalCount.Size = new System.Drawing.Size(81, 23);
+            this.lblActualTotalCount.TabIndex = 8;
+            this.lblActualTotalCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalValue.Location = new System.Drawing.Point(466, 137);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(81, 29);
+            this.lblTotalValue.TabIndex = 9;
+            this.lblTotalValue.Text = "Total Value";
+            this.lblTotalValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice2Count
+            // 
+            this.lblDice2Count.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDice2Count.Location = new System.Drawing.Point(130, 360);
+            this.lblDice2Count.Name = "lblDice2Count";
+            this.lblDice2Count.Size = new System.Drawing.Size(73, 23);
+            this.lblDice2Count.TabIndex = 10;
+            this.lblDice2Count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice3Count
+            // 
+            this.lblDice3Count.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDice3Count.Location = new System.Drawing.Point(315, 153);
+            this.lblDice3Count.Name = "lblDice3Count";
+            this.lblDice3Count.Size = new System.Drawing.Size(73, 23);
+            this.lblDice3Count.TabIndex = 11;
+            this.lblDice3Count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(466, 312);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(81, 35);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(466, 271);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(81, 35);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmNumbersCalculations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblDice3Count);
+            this.Controls.Add(this.lblDice2Count);
+            this.Controls.Add(this.lblTotalValue);
+            this.Controls.Add(this.lblActualTotalCount);
+            this.Controls.Add(this.lblActualTotalValue);
+            this.Controls.Add(this.lblTotalCount);
+            this.Controls.Add(this.lblDice4Count);
+            this.Controls.Add(this.lblDice1Count);
+            this.Controls.Add(this.pbxDice3);
+            this.Controls.Add(this.pbxDice4);
+            this.Controls.Add(this.pbxDice1);
+            this.Controls.Add(this.pbsDice2);
+            this.Name = "frmNumbersCalculations";
+            this.Text = "Numbers and Calculations";
+            ((System.ComponentModel.ISupportInitialize)(this.pbsDice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDice3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbsDice2;
+        private System.Windows.Forms.PictureBox pbxDice1;
+        private System.Windows.Forms.PictureBox pbxDice4;
+        private System.Windows.Forms.PictureBox pbxDice3;
+        private System.Windows.Forms.Label lblDice1Count;
+        private System.Windows.Forms.Label lblDice4Count;
+        private System.Windows.Forms.Label lblTotalCount;
+        private System.Windows.Forms.Label lblActualTotalValue;
+        private System.Windows.Forms.Label lblActualTotalCount;
+        private System.Windows.Forms.Label lblTotalValue;
+        private System.Windows.Forms.Label lblDice2Count;
+        private System.Windows.Forms.Label lblDice3Count;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Week 1_B/Week 1_B.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_1_B
+{
+    public partial class frmNumbersCalculations : Form
+    {
+        //Declare and Initialize constant fields.
+        private const int DICE_1 = 1;
+        private const int DICE_2 = 2;
+        private const int DICE_3 = 3;
+        private const int DICE_4 = 4;
+
+        //Declare and initialize field variables to hold the total value. 
+        private int total_value = 0;
+        private int total_clickcount = 0;
+        private int local_click1 = 0;
+        private int local_click2 = 0;
+        private int local_click3 = 0;
+        private int local_click4 = 0;
+
+        public frmNumbersCalculations()
+        {
+            InitializeComponent();
+        }
+
+        private void pbxDice1_Click(object sender, EventArgs e)
+        {
+            //Declare and initialize constant fields for counting clicks. 
+            const int DICE1_click = 1; 
+
+            //Add local click1 count and display in proper Dice 1 label. 
+            local_click1 += DICE1_click;
+            lblDice1Count.Text = local_click1.ToString();
+
+            //Add total number of clicks to total click count variable. 
+            total_clickcount += DICE1_click;
+            //Display the total click count in the final click count label. 
+            lblActualTotalCount.Text = total_clickcount.ToString();
+
+            //Add value of Dice 1 to the total value of all dice being clicked (total number of pips). 
+            total_value += DICE_1;
+            //Display this total dice value (total pips) in proper label, formatted as an int.
+            lblActualTotalValue.Text = total_value.ToString();
+
+        }
+
+        private void pbsDice2_Click(object sender, EventArgs e)
+        {
+            //Declare and initialize constant fields for counting clicks. 
+            const int DICE2_click = 1;
+
+            //Add local click1 count and display in proper Dice 1 label.
+            local_click2 += DICE2_click;
+            lblDice2Count.Text = local_click2.ToString();
+
+            //Add total number of clicks to total click count variable. 
+            total_clickcount += DICE2_click;
+            //Display the total click count in the final click count label. 
+            lblActualTotalCount.Text = total_clickcount.ToString();
+
+            //Add value of Dice 1 to the total value of all dice being clicked (total number of pips). 
+            total_value += DICE_2;
+            //Display this total dice value (total pips) in proper label, formatted as an int. 
+            lblActualTotalValue.Text = total_value.ToString();
+        }
+
+        private void pbxDice3_Click(object sender, EventArgs e)
+        {
+            //Declare and initialize constant fields for counting clicks. 
+            const int DICE3_click = 1;
+
+            //Add local click1 count and display in proper Dice 1 label.
+            local_click3 += DICE3_click;
+            lblDice3Count.Text = local_click3.ToString();
+
+            //Add total number of clicks to total click count variable. 
+            total_clickcount += DICE3_click;
+            //Display the total click count in the final click count label. 
+            lblActualTotalCount.Text = total_clickcount.ToString();
+
+            //Add value of Dice 1 to the total value of all dice being clicked (total number of pips). 
+            total_value += DICE_3;
+            //Display this total dice value (total pips) in proper label, formatted as an int. 
+            lblActualTotalValue.Text = total_value.ToString();
+
+
+        }
+
+        private void pbxDice4_Click(object sender, EventArgs e)
+        {
+            //Declare and initialize constant fields for counting clicks. 
+            const int DICE4_click = 1;
+
+            //Add local click1 count and display in proper Dice 1 label.
+            local_click4 += DICE4_click;
+            lblDice4Count.Text = local_click4.ToString();
+
+            //Add total number of clicks to total click count variable. 
+            total_clickcount += DICE4_click;
+            //Display the total click count in the final click count label. 
+            lblActualTotalCount.Text = total_clickcount.ToString();
+
+            //Add value of Dice 1 to the total value of all dice being clicked (total number of pips). 
+            total_value += DICE_4;
+            //Display this total dice value (total pips) in proper label, formatted as an int. 
+            lblActualTotalValue.Text = total_value.ToString();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Clears all labels that contain text. 
+            lblDice1Count.Text = "";
+            lblDice2Count.Text = "";
+            lblDice3Count.Text = "";
+            lblDice4Count.Text = "";
+            lblActualTotalValue.Text = "";
+            lblActualTotalCount.Text = "";
+            total_value = 0;
+            total_clickcount = 0;
+            local_click1 = 0;
+            local_click2 = 0;
+            local_click3 = 0;
+            local_click4 = 0;
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Closes the form entirely. 
+            this.Close();
+        }
+    }
+}
